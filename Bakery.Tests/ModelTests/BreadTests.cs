0191c264e27825117fdbe43c7646e82d837c2c2a
OLGON92 <ogon318@gmail.com>
2022-11-28 14:01:23 -0800

    conduct test to get qty of bread

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,18 @@ namespace Bakery.Tests
       Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+
+    public void GetQty_ReturnBreadLoafAmt_Int()
+    {
+      //Arrange
+      int breadLoafAmt = 4;
+      Bread newBread = new Bread(breadLoafAmt);
+      //Act
+      int resultAmt = newBread.BreadTotalAmt;
+      //Assert
+      Assert.AreEqual(breadLoafAmt, resultAmt);
+    }
   } 
 }
