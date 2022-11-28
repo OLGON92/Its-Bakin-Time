434f60150c886b21c5fdc1dae0f8e231ad39f61c
OLGON92 <ogon318@gmail.com>
2022-11-28 14:26:37 -0800

    pass test to get price of 1 loaf

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,13 +2,24 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    public int BreadLoafCost {get; set;}
     public int BreadLoafAmt {get; set;}
+    public int BreadLoafCost {get; set;}
 
     public Bread(int breadLoafAmt)
     {
       BreadLoafAmt = breadLoafAmt;
       BreadLoafCost = 5;
+    }
+
+    public int GetBreadPrice()
+    {
+      int totalPrice = 0;
+      int totalLoaves = BreadLoafAmt;
+      if(totalLoaves == 1)
+      {
+        return totalPrice = 5;
+      }
+      else return 6;
     }
   }
 }
