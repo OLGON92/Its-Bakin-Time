c162e61e85ce5124171134f34a2545a674dcaf75
OLGON92 <ogon318@gmail.com>
2022-11-28 01:33:07 -0800

    pass first test & commit

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,13 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public int BreadLoafCost {get; set;}
+    public int BreadLoafAmt {get; set;}
+
+    public Bread(int breadLoafAmt)
+    {
+      BreadLoafAmt = breadLoafAmt;
+      BreadLoafCost = 5;
+    }
   }
 }
