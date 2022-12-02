10b16953212a507bb26118f61b48722392d8041d
OLGON92 <ogon318@gmail.com>
2022-12-01 19:27:27 -0800

    conduct test to get pastry object

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    
+  } 
+}
