fa3d8218533b1cd0f23bc18d517e03bde733d9f3
OLGON92 <ogon318@gmail.com>
2022-12-01 20:45:57 -0800

    pass tests to get price of 1,2,3,4,5, & 6 pastries

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -10,6 +10,27 @@ namespace Bakery.Models
       PastryCost = 2;
     }
 
-    //public int GetPastryPrice()
+    public int GetPastryPrice()
+    {
+      int totalPrice = 0;
+      int pastryTotal = PastryAmt;
+
+      if(pastryTotal <= 2)
+      {
+        totalPrice = (pastryTotal * PastryCost);
+        return totalPrice;
+      }
+      else if(pastryTotal >= 3 && pastryTotal <= 5)
+      {
+        totalPrice = ((pastryTotal * PastryCost) - 1);
+        return totalPrice;
+      }
+      else
+      {
+        totalPrice = ((pastryTotal * PastryCost) - 2);
+        return totalPrice;
+      }
+
+    }
   }
 }
