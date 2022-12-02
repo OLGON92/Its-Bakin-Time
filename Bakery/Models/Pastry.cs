001e74423740b4e7ae64bad3f2f96fd613a37831
OLGON92 <ogon318@gmail.com>
2022-12-01 19:29:58 -0800

    pass first test

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,6 +1,15 @@
 namespace Bakery.Models
 {
   public class Pastry{
-    
+    public int PastryAmt {get; set;}
+    public int PastryCost {get; set;}
+
+    public Pastry(int pastryAmt)
+    {
+      PastryAmt = pastryAmt;
+      PastryCost = 2;
+    }
+
+    //public int GetPastryPrice()
   }
 }
